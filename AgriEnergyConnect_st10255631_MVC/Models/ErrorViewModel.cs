563c597ce5d6e928a5a9e7f347e39382378c5329
Joshua Dean Shields <shields.joshua23@gmail.com>
2025-04-02 14:22:35 +0200

    start of code

---
+++ AgriEnergyConnect_st10255631_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AgriEnergyConnect_st10255631_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
